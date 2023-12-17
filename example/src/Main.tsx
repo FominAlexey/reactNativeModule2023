51c5258c9b522222a49236177eca982f9a63ceaa
RadtsevichAlexey <67689263+RadtsevichAlexey@users.noreply.github.com>
2023-12-17 09:00:58 +0300

    Добавлены уведомления и навигация

--- example/src/Main.tsx
+++ example/src/Main.tsx
@@ -10,18 +10,14 @@ import {
 } from 'react-native';
 import { NativeModules } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Main from './Main';
+import App from './App';
 
 const { NotificationModule, PinCodeModule } = NativeModules;
 
-export default function App() {
+export default function Main() {
   const navigation = useNavigation();
   const [pinCodeInput, setPinCodeInput] = React.useState('');
   const [message, setMessage] = React.useState('');
-
-  React.useEffect(() => {
-    // Здесь ваш useEffect
-  }, []);
 
   const checkApplicationPermission = async () => {
     if (Platform.OS === 'android') {
@@ -33,41 +29,31 @@ export default function App() {
     }
   };
 
-  
+  React.useEffect(() => {
+    checkApplicationPermission().then(() => {
+        NotificationModule.sendNotification(
+          'Добро пожаловать!',
+          ''
+        );
+    });
+  }, []);
 
-  const signin = () => {
-    PinCodeModule.checkPinCode(pinCodeInput)
-      .then((isCorrect: any) => {
-        if (isCorrect) {
-          setMessage('Пин-код верный');
-          setPinCodeInput('');
-          navigation.navigate('Main'); // TODO скорее всего не работает
+  const handleSavePinCode = () => {
+    PinCodeModule.savePinCode(pinCodeInput);
+    checkApplicationPermission().then(() => {
+      NotificationModule.sendNotification(
+        'Пин-код сохранен',
+        ''
+      );
+    });
+    setMessage('Пин-код сохранен');
+    setPinCodeInput('');
+  };
 
-          // Разблокируйте приложение или выполните другие необходимые действия
-        } else {
-          setMessage('Неверный пин-код');
-          checkApplicationPermission().then(() => {
-            NotificationModule.sendNotification(
-              'Пин-код не верный',
-              ''
-            );
-            setPinCodeInput('');
-          });
-          // Обработка неверного пин-кода
-        }
-        setPinCodeInput('');
-      })
-      .catch((error: any) => {
-        checkApplicationPermission().then(() => {
-          NotificationModule.sendNotification(
-            'Ошибка проверки пин-кода',
-            error
-          );
-        });
-        setMessage('Ошибка проверки пин-кода: ' + error);
-        console.error('Ошибка проверки пин-кода:', error);
-        setPinCodeInput('');
-      });
+  const logout = () => {
+    setMessage('Выполнен выход');
+    setPinCodeInput('');
+    navigation.navigate('App'); // TODO скорее всего не работает
   };
 
   return (
@@ -82,7 +68,8 @@ export default function App() {
         maxLength={4}
       />
       <View style={styles.buttonsContainer}>
-        <Button onPress={signin} title="Войти" />
+        <Button onPress={handleSavePinCode} title="Сохранить пин-код" />
+        <Button onPress={logout} title="Выйти" />
       </View>
       <Text>{message}</Text>
     </View>
